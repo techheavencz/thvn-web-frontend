03c28bc9fa65ce8e5cf4f77afb78a7790fcb0c37
Barča Vobořilová <46422600+voborilovab@users.noreply.github.com>
2019-06-01 18:33:09 +0200

    Connected with API

--- script/fetchEvents.js
+++ script/fetchEvents.js
@@ -1,6 +1,7 @@
 import {Event} from "./Event.js";
+const API_URL = `http://localhost:3000`;
 
 export async function fetchEvents() {
-    const response = await fetch("./api/events.json");
-    return (await response.json()).map(eventData => new Event(eventData));
+    const response = await fetch(`${API_URL}/events/1777732895836985`);
+    return (await response.json()).data.map(eventData => new Event(eventData));
 }
--- script/index.js
+++ script/index.js
@@ -6,7 +6,7 @@ export async function main(){
     const containerElement = document.getElementById("events");
     const events = await fetchEvents();
     const eventPrev = new EventPrev(events[0]);
-    var x = 0;
+    let x = 0;
     for(const event of events){
         if(x!=0){
             const eventBox = new EventBox(event, containerElement);
