a2a8ce39cbfb5bd13f6b1bca0840906773836343
Barča Vobořilová <46422600+voborilovab@users.noreply.github.com>
2019-06-05 12:19:02 +0200

    Add past events

--- script/index.js
+++ script/index.js
@@ -3,7 +3,8 @@ import {EventBox} from "./EventBox.js";
 import {EventPrev} from "./EventPrev.js";
 
 export async function main(){
-    const containerElement = document.getElementById("events");
+    const containerElement = document.getElementById("eventsNext");
+    const containerEl = document.getElementById("eventsPast");
     const events = await fetchEvents();
     const date = new Date();
     let eventsAct = new Array();
@@ -16,8 +17,12 @@ export async function main(){
     }
     eventsAct = eventsAct.reverse();
     const eventPrev = new EventPrev(eventsAct[0]);
+    events.splice(0, eventsAct.length);
     eventsAct.splice(0, 1);
     for(const event of eventsAct){
         const eventBox = new EventBox(event, containerElement);
+    }
+    for(const event of events){
+        const eventBox = new EventBox(event, containerEl);
     }
 }
