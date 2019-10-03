d5075cd09a683e3d1c9c7fe2f18fb3a8c10ef4b5
Michal Vašíček <me@michalvasicek.cz>
2019-10-04 01:53:40 +0200

    dow & time in dates, fixes #49

--- public/script/Helpers.js
+++ public/script/Helpers.js
@@ -3,3 +3,17 @@ export function htmlEscape(input) {
     el.textContent = input;
     return el.innerHTML;
 }
+
+export function formatDate(date) {
+    const month = date.getMonth() + 1;
+    const day = days[date.getDay() - 1];
+    const time = `${pad(date.getUTCHours(), 2)}:${pad(date.getUTCMinutes(), 2)}`;
+    return `${day} ${date.getDate()}. ${month}. ${time}`;
+}
+
+export function pad(n, width) {
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
+
+export const days = ['pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota', 'neděle'];
