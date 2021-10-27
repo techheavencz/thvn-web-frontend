891890bf2a7d7d8672ed5c29cad0182dcee84126
Daniel Brada <danik.brada@seznam.cz>
2021-10-27 16:59:50 +0200

    cleanup

--- public/script/fetchEvents.js
+++
@@ -1,8 +0,0 @@
-import {Event} from "./Event.js";
-
-const API_URL = `https://events.thvn-dev.cz`;
-
-export async function fetchEvents() {
-    const response = await fetch(`${API_URL}/events/TechHeavenCZ`);
-    return (await response.json()).data.map(eventData => new Event(eventData));
-}
